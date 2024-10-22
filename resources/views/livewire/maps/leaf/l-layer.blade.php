a98de181430b2e4f3ee12549c33b32b7064e4625
Adi Arta Wibawa <75978314+adiartawibawa@users.noreply.github.com>
2024-10-22 10:05:04 +0800

    change checkbox into switch button

--- resources/views/livewire/maps/leaf/l-layer.blade.php
+++ resources/views/livewire/maps/leaf/l-layer.blade.php
@@ -1,13 +1,23 @@
 <div>
     <h3 class="text-lg font-semibold mb-4">Layer</h3>
-    <ul class="space-y-2">
+    <div class="space-y-2">
         @foreach ($layers as $layer)
-            {{-- @dd($layer) --}}
-            <li class="flex items-center">
-                <input type="checkbox" wire:click="toggleLayerVisibility('{{ $layer['name'] }}')"
-                    {{ $layer['visible'] ? 'checked' : '' }}>
-                {{ $layer['name'] }}
-            </li>
+            <div class="flex items-center justify-between w-full">
+                <!-- Switch -->
+                <input type="checkbox" id="switch-{{ $layer['name'] }}"
+                    wire:click="toggleLayerVisibility('{{ $layer['name'] }}')" {{ $layer['visible'] ? 'checked' : '' }}
+                    class="sr-only" />
+                <label for="switch-{{ $layer['name'] }}" class="flex items-center cursor-pointer">
+                    <span class="text-black">{{ $layer['name'] }}</span>
+                    <div class="relative ml-3">
+                        <div class="block bg-gray-400 w-12 h-6 rounded-full"></div>
+                        <div
+                            class="dot absolute left-0 top-0 bg-white w-6 h-6 rounded-full transition transform {{ $layer['visible'] ? 'translate-x-full bg-primary' : '' }}">
+                        </div>
+                    </div>
+                </label>
+            </div>
         @endforeach
-    </ul>
+    </div>
+
 </div>
